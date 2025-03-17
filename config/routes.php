09aa9ffb5459b926b684ab677f7da206e7f52544
Julius Derigs <julius.derigs@mail.de>
2025-03-17 18:05:50 +0100

    Schreiben der Routen.

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,47 @@
 <?php
 
-$routes[] = ['BaseController', 'welcome'];
+$routes[] = ['Pages', 'login'];
+
+/*
+ * Broadcasts
+ */
+$routes['broadcasts'] = [ 'Broadcasts', 'index' ];
+$routes['show-broadcast'] = [ 'Broadcasts', 'show' ];
+$routes['create-broadcast'] = [ 'Broadcasts', 'create' ];
+$routes['update-broadcast'] = [ 'Broadcasts', 'update' ];
+$routes['delete-broadcast'] = [ 'Broadcasts', 'delete' ];
+
+/*
+ * Pages
+ */
+$routes['login'] = [ 'Pages', 'login' ];
+$routes['logout'] = [ 'Pages', 'logout' ];
+
+/*
+ * Platforms
+ */
+$routes['platforms'] = [ 'Platforms', 'index' ];
+$routes['show-platform'] = [ 'Platforms', 'show' ];
+$routes['create-platform'] = [ 'Platforms', 'create' ];
+$routes['update-platform'] = [ 'Platforms', 'update' ];
+$routes['delete-platform'] = [ 'Platforms', 'delete' ];
+
+/*
+ * Types
+ */
+$routes['types'] = [ 'Types', 'index' ];
+$routes['show-type'] = [ 'Types', 'show' ];
+$routes['create-type'] = [ 'Types', 'create' ];
+$routes['update-type'] = [ 'Types', 'update' ];
+$routes['delete-type'] = [ 'Types', 'delete' ];
+
+/*
+ * Users
+ */
+$routes['users'] = [ 'Users', 'index' ];
+$routes['show-user'] = [ 'Users', 'show' ];
+$routes['create-user'] = [ 'Users', 'create' ];
+$routes['update-user'] = [ 'Users', 'update' ];
+$routes['delete-user'] = [ 'Users', 'delete' ];
 
 return $routes;
